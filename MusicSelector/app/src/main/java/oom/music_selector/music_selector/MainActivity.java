fd4662c25c7886999e71df495531f50957beec17
Jeong-minji <alswl4152@naver.com>
2017-12-14 19:07:37 +0900

    프로젝트 생성

---
+++ MusicSelector/app/src/main/java/oom/music_selector/music_selector/MainActivity.java
@@ -0,0 +1,15 @@
+package oom.music_selector.music_selector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import oom.music_selector.musicselector.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

b93a05678192b9d544c719021b103fdd4b753e86
Jeong-minji <alswl4152@naver.com>
2017-12-27 18:52:13 +0900

    music player 2차 점검

--- MusicPlayer/app/src/main/java/com/music_player/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/music_player/musicplayer/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
